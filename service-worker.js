3b6301e300aaebd2a765cb967abc7b439b4f7656
gm-yatsu <kenichi.nakada@accor.com>
2026-06-12 15:59:09 +0900

    Update service-worker.js
    Clear old cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,24 @@
-const CACHE='yatsu-rpg-firebase-v3';
-const ASSETS=['./','./index.html','./manifest.json','./firebase-config.js','./firebase-sync.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "yatsu-rpg-firebase-v5";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+  );
+});
