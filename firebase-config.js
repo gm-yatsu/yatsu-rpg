f4db37277ceca97581a44ed1b0582762f59fb1b2
gm-yatsu <kenichi.nakada@accor.com>
2026-06-12 15:56:38 +0900

    Update firebase-config.js
    Fix Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebaseコンソールで発行された設定値に置き換えてください。
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCwkJAjBcLxuM5PmAJWogZRSl3pI9KHKcU",
   authDomain: "yatsu-rpg.firebaseapp.com",
@@ -8,6 +7,5 @@ const FIREBASE_CONFIG = {
   appId: "1:743176971942:web:2fc6ac30c2f44742173667"
 };
 
-// 管理画面へのアクセスを許可するGoogleアカウントのメールアドレス。
-// firestore.rules内のメールアドレスも同じ値へ変更してください。
+
 const ADMIN_EMAIL = "yature567@gmail.com";
